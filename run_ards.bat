29c5344b53cf103dfc85288651464fbc2fd0e69d
eddin022 <eddin022@umn.edu>
2026-06-10 17:33:47 -0500

    ahrf fix

--- run_ards.bat
+++ run_ards.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-set "ENV=C:\Users\eddin022\AppData\Local\miniconda3\envs\myR"
+set "ENV=C:\Users\cas28278\AppData\Local\miniconda3\envs\myR"
 set "CONDA_PREFIX=%ENV%"
 set "PATH=%ENV%\Library\bin;%ENV%\Scripts;%ENV%\bin;%ENV%\lib\R\bin\x64;%PATH%"
 
-"%ENV%\lib\R\bin\x64\Rscript.exe" "ards_classifier.R"
+"%ENV%\lib\R\bin\x64\Rscript.exe" "ards_classifier.R"
